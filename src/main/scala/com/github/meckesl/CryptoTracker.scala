fc2adb6f200105cb623a4fcc09ccaf44e8909365
meckesl <louis.meckes@gmx.com>
2022-04-06 07:13:09 +0200

    wip

--- src/main/scala/com/github/meckesl/CryptoTracker.scala
+++ src/main/scala/com/github/meckesl/CryptoTracker.scala
@@ -1,7 +1,30 @@
 package com.github.meckesl
 
+import com.litesoftwares.coingecko.domain.Coins.CoinFullData
+import com.litesoftwares.coingecko.impl.CoinGeckoApiClientImpl
+
+import scala.jdk.CollectionConverters.*
+import java.util.Date
+
 object CryptoTracker extends App {
 
-  println("OK")
+  val client = new CoinGeckoApiClientImpl
+  val allCoins = client.getCoinList.asScala
+  println(allCoins.length + " coins in database")
+
+  case class Holding(symbol: String, value: Double)
+  case class Result(symbol: String, USDValue: Double, date: Date)
+
+  def USDValue(c: CoinFullData) = c.getTickers.asScala.filter(_.getTarget.toUpperCase.equals("USD")).headOption.map(_.getLast).getOrElse(Double.NaN)
+
+  val coins = Seq(Holding("BTC", 1), Holding("DOT", 2))
+
+  val results =
+    coins
+      .map(c=> allCoins.filter(_.getSymbol.toLowerCase.equals(c.symbol.toLowerCase)).head)
+      .map(d=> client.getCoinById(d.getId))
+      .map(e=> Result(e.getSymbol, USDValue(e), new Date))
+
+  print(results.mkString("\n"))
 
 }
--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,16 +1,27 @@
 import com.litesoftwares.coingecko.CoinGeckoApiClient
+import com.litesoftwares.coingecko.constant.Currency
 import com.litesoftwares.coingecko.domain.Coins.CoinList
 import com.litesoftwares.coingecko.impl.CoinGeckoApiClientImpl
 
 import java.util
 import java.util.List
+import scala.jdk.CollectionConverters._
 
 // For more information on writing tests, see
 // https://scalameta.org/munit/docs/getting-started.html
 class MySuite extends munit.FunSuite {
+
+  val client = new CoinGeckoApiClientImpl
+
   test("coinlist") {
-    val client = new CoinGeckoApiClientImpl
     val coinList = client.getCoinList
     println(s"${coinList.size()} coins found")
   }
+
+  test("eurMarkets") {
+    val markets = client.getCoinMarkets(Currency.EUR).asScala
+    println(s"${markets.length} EUR markets found")
+    println(markets.map(_.getSymbol).mkString(","))
+  }
+
 }
