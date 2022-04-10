ba565a27741d391936b9b452151d4b7edcaf2a8e
meckesl <louis.meckes@gmx.com>
2022-04-10 17:57:47 +0200

    wip

--- src/main/scala/com/github/meckesl/CryptoTracker.scala
+++ src/main/scala/com/github/meckesl/CryptoTracker.scala
@@ -41,12 +41,15 @@ object CryptoTracker extends App {
       Holding("ftm", 500)
     )
 
-  val res = (
+  val res =
     holdings
+      .groupBy(_.symbol).map { case (s,a) => Holding(s, a.map(_.amount).sum) }
       .map(h => (h, allCoins.filter(_.getSymbol.toLowerCase.equals(h.symbol.toLowerCase)).head))
       .map(h => (h._1, client.getCoinById(h._2.getId)))
       .map(h => Holding(h._1.symbol, h._1.amount * marketValue(h._2)))
-  )
+      .toList
+      .sortBy(_.amount)
+      .reverse
 
   println(res.mkString("\n"))
   println(s"Total=${res.foldLeft(0.0){(acc, num) => acc + (num.amount)}}")
